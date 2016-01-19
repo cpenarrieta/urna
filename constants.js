55196b3e61b2ac94ae553d8e8c4d3c6ecfd9ca68
Cristian Penarrieta <cristian.penarrieta@gmail.com>
2016-01-19 15:52:24 -0800

    refactor constants file

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  LINKEDIN_CLIENT_ID: process.env.LINKEDIN_CLIENT_ID || "insert key here",
-  LINKEDIN_CLIENT_SECRET: process.env.LINKEDIN_CLIENT_SECRET || "inser key here",
+  LINKEDIN_CLIENT_ID: process.env.LINKEDIN_CLIENT_ID,
+  LINKEDIN_CLIENT_SECRET: process.env.LINKEDIN_CLIENT_SECRET,
   API_URL: process.env.API_URL || "http://127.0.0.1:8000",
   MONGOLAB_URI: process.env.MONGOLAB_URI || "mongodb://localhost/urna"
 };
